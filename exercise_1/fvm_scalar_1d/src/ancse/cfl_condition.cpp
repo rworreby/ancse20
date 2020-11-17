245346b42c081c327b64aca0aeb61b4159869ccf
Robin Worreby <rworreby@student.ethz.ch>
2020-11-17 19:45:32 +0100

    Implement cfl condition

--- exercise_1/fvm_scalar_1d/src/ancse/cfl_condition.cpp
+++ exercise_1/fvm_scalar_1d/src/ancse/cfl_condition.cpp
@@ -1,5 +1,5 @@
 #include <ancse/cfl_condition.hpp>
-
+#include <limits>
 
 //----------------StandardCFLConditionDefnBegin----------------
 StandardCFLCondition::StandardCFLCondition(const Grid &grid,
@@ -8,12 +8,21 @@ StandardCFLCondition::StandardCFLCondition(const Grid &grid,
     : grid(grid), model(model), cfl_number(cfl_number) {}
 
 double StandardCFLCondition::operator()(const Eigen::VectorXd &u) const {
+    assert(cfl_number < 1.0);
 
     auto n_cells = grid.n_cells;
     auto n_ghost = grid.n_ghost;
 
+    double max_fp{ std::numeric_limits<double>::lowest() };
+    for (int i = n_ghost-1; i < n_cells - n_ghost; i++) {
+        if(max_fp < model.max_eigenvalue(u(i))){
+            max_fp = model.max_eigenvalue(u(i));
+        }
+    }
 
-    return 0.0;
+    double const dt{ cfl_number * grid.dx / max_fp };
+
+    return dt;
 }
 //----------------StandardCFLConditionDefnEnd----------------
 
