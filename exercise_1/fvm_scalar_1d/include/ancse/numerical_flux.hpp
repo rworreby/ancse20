eaf5436c54de4c65035bdb2bebaeb87b77ecaaf2
Robin Worreby <rworreby@student.ethz.ch>
2020-11-20 10:25:39 +0100

    Implement Roe and Rusanov flux

--- exercise_1/fvm_scalar_1d/include/ancse/numerical_flux.hpp
+++ exercise_1/fvm_scalar_1d/include/ancse/numerical_flux.hpp
@@ -35,7 +35,7 @@ class CentralFlux {
 };
 
 
-//----------------FluxLFBegin----------------
+//---------------FluxLFBegin----------------
 /// Lax-Friedrichs numerical flux.
 /** This flux works for any model. */
 class LaxFriedrichs {
@@ -69,7 +69,104 @@ class LaxFriedrichs {
     Grid grid;
     Model model;
 };
-//----------------FluxLFEnd----------------
+//----------------FluxLFEnd-----------------
 
+
+//---------------FluxRusBegin---------------
+/// Rusanov's numerical flux.
+class Rusanov {
+  public:
+    Rusanov(const Model &model) : model(model) {}
+
+    double operator()(double uL, double uR) const {
+        auto fL = model.flux(uL);
+        auto fR = model.flux(uR);
+
+        double flux{ 0.5 * (fL + fR) };
+        flux -= (0.5 * std::max(std::abs(model.max_eigenvalue(uL)),
+                 std::abs(model.max_eigenvalue(uR)))
+                ) * (uR - uL);
+        return flux;
+    }
+
+  private:
+    Model model;
+};
+//----------------FluxRusEnd----------------
+
+
+//---------------FluxRoeBegin---------------
+/// Roe numerical flux.
+class Roe {
+  public:
+    Roe(const Model &model) : model(model) {}
+
+    double operator()(double uL, double uR) const {
+        auto fL = model.flux(uL);
+        auto fR = model.flux(uR);
+
+        double a_hat{ 0.0 };
+        if(uL != uR){
+            a_hat = (fR - fL) / (uR - uL);
+        }
+        else{
+            a_hat = model.max_eigenvalue(uL);
+        }
+
+        return a_hat < 0 ? fR : fL;
+    }
+
+  private:
+    Model model;
+};
+//----------------FluxRoeEnd----------------
+
+
+//-------------FluxGodunovBegin-------------
+/// Godunov numerical flux.
+class Godunov {
+  public:
+    // Note: This version is a bit tricky. A numerical flux should be
+    //       a function of the two trace values at the interface, i.e. what we
+    //       call `uL`, `uR`. However, it requires 'dt' and 'dx'. Therefore,
+    //       these need to be made available to the flux. This is one of the
+    //       reasons why `SimulationTime`.
+    Godunov(const Model &model) : model(model) {}
+
+    double operator()(double uL, double uR) const {
+        auto fL = model.flux(uL);
+        auto fR = model.flux(uR);
+
+        // TODO: implement Godunov Flux
+    }
+
+  private:
+    Model model;
+};
+//--------------FluxGodunovEnd--------------
+
+
+//---------------FluxEOBegin----------------
+/// Engquist-Osher numerical flux.
+class EngquistOsher {
+  public:
+    // Note: This version is a bit tricky. A numerical flux should be
+    //       a function of the two trace values at the interface, i.e. what we
+    //       call `uL`, `uR`. However, it requires 'dt' and 'dx'. Therefore,
+    //       these need to be made available to the flux. This is one of the
+    //       reasons why `SimulationTime`.
+    EngquistOsher(const Model &model) : model(model) {}
+
+    double operator()(double uL, double uR) const {
+        auto fL = model.flux(uL);
+        auto fR = model.flux(uR);
+
+        // TODO: implement EO Flux
+    }
+
+  private:
+    Model model;
+};
+//----------------FluxEOEnd-----------------
 
 #endif // FVMSCALAR1D_NUMERICAL_FLUX_HPP
