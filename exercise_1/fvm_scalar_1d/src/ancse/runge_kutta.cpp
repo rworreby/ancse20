fefd733c12c6634115b9243c9ab245d81653bec4
Robin Worreby <rworreby@student.ethz.ch>
2020-11-20 10:27:18 +0100

    Fix boundary issue with SSP2

--- exercise_1/fvm_scalar_1d/src/ancse/runge_kutta.cpp
+++ exercise_1/fvm_scalar_1d/src/ancse/runge_kutta.cpp
@@ -53,7 +53,7 @@ operator()(Eigen::VectorXd &u1, const Eigen::VectorXd &u0, double dt) const {
     // You can reduce memory consumption by using `u1` as the temporary
     // buffer `u_star`.
 
-    //(*boundary_condition)(u0);
+    (*boundary_condition)(u1);
     (*rate_of_change)(dudt, u0);
     u_star = u0 + dt * dudt;
     u1 = u_star + dt * dudt;
