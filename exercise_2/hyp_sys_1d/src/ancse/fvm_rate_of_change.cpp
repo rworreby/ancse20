8cd9dc8fa00e7ab6f7515c99e2becb3e59bc2ed1
Robin Worreby <rworreby@student.ethz.ch>
2020-12-18 21:01:53 +0100

    Fix registering of reconstruction

--- exercise_2/hyp_sys_1d/src/ancse/fvm_rate_of_change.cpp
+++ exercise_2/hyp_sys_1d/src/ancse/fvm_rate_of_change.cpp
@@ -20,11 +20,12 @@ deduce_numerical_flux(const nlohmann::json &config,
                       const Reconstruction &reconstruction)
 {
     REGISTER_NUMERICAL_FLUX("central_flux", CentralFlux, CentralFlux(model))
-    REGISTER_NUMERICAL_FLUX("lax_friedrichs", CentralFlux, LaxFriedrichs(model))
-    REGISTER_NUMERICAL_FLUX("rusanov", CentralFlux, Rusanov(model))
-    REGISTER_NUMERICAL_FLUX("roe", CentralFlux, Roe(model))
-    REGISTER_NUMERICAL_FLUX("hll", CentralFlux, HLL(model))
-    REGISTER_NUMERICAL_FLUX("hllc", HLLCEuler(model))
+    REGISTER_NUMERICAL_FLUX("lax_friedrichs",
+                            LaxFriedrichs,
+                            LaxFriedrichs(grid, model, simulation_time))
+    REGISTER_NUMERICAL_FLUX("rusanov", Rusanov, Rusanov(model))
+    REGISTER_NUMERICAL_FLUX("roe", Roe, Roe(model))
+    REGISTER_NUMERICAL_FLUX("hll", HLL, HLL(model))
 
     if (model->get_name().compare("euler") == 0) {
         auto model_euler = std::dynamic_pointer_cast<Euler>(model);
@@ -52,7 +53,39 @@ std::shared_ptr<RateOfChange> make_fvm_rate_of_change(
 {
     REGISTER_RECONSTRUCTION("o1", PWConstantReconstruction{})
 
-    // Register piecewise linear reconstructions.
+    if (config["reconstruction_variable"] == "primitive") {
+        REGISTER_RECONSTRUCTION(
+            "minmod",
+            (PWLinearReconstruction<MinMod, Primitive>(model, MinMod{}))
+        )
+        REGISTER_RECONSTRUCTION(
+            "superbee",
+            (PWLinearReconstruction<SuperBee, Primitive>(model, SuperBee{}))
+        )
+        REGISTER_RECONSTRUCTION(
+            "monotonized_central",
+            (PWLinearReconstruction<MonotonizedCentral, Primitive>
+                (model, MonotonizedCentral{})
+            )
+        )
+    }
+    else if (config["reconstruction_variable"] == "conserved") {
+        REGISTER_RECONSTRUCTION(
+            "minmod",
+            (PWLinearReconstruction<MinMod, Conserved>(MinMod{}))
+        )
+        REGISTER_RECONSTRUCTION(
+            "superbee",
+            (PWLinearReconstruction<SuperBee, Conserved>(SuperBee{}))
+        )
+        REGISTER_RECONSTRUCTION(
+            "monotonized_central",
+            (PWLinearReconstruction<MonotonizedCentral, Conserved>
+                (MonotonizedCentral{})
+            )
+        )
+    }
+
 
     throw std::runtime_error(fmt::format(
         "Unknown reconstruction. [{}]", std::string(config["reconstruction"])));
